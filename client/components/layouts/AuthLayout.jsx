0cdab0ffa2437ed79a954530d10f35628e6b4419
ismaelnelro <712020:5a135d83-d3f1-4499-8797-b18897146ebf@bots.bitbucket.org>
2023-06-20 13:14:32 -0300

    Estructura de login y registro

---
+++ client/components/layouts/AuthLayout.jsx
@@ -0,0 +1,22 @@
+import Head from 'next/head'
+import React from 'react'
+
+
+
+
+
+export const AuthLayouts = ({ children, title }) => {
+  return (
+    <>
+      <Head>
+        <title>{title}</title>
+      </Head>
+
+      <main >
+        <div className='flex-col items-center h-screen max-w-max'>
+          {children}
+        </div>
+      </main>
+    </>
+  )
+}
