0cdc18cd0acfc69b2ecce71538e7d74401da3824
cristian1534 <cmachuca32@gmail.com>
2023-08-17 19:04:12 +0000

    Redis applied on youtube API, Bootcamp Doc API.

--- server/src/controllers/tutorial/react.js
+++ server/src/controllers/tutorial/react.js
@@ -1,22 +1,33 @@
+const redisConnection = require("../../database/redis");
 const { searchVideos } = require("../../services/videos");
 
 exports.reactVideos = async (req, res) => {
   try {
+    const redisClient = await redisConnection();
+    const cachedData = await redisClient.get("reactVideosCache");
+
+    if (cachedData) {
+      const parsedData = JSON.parse(cachedData);
+      return res.status(200).json(parsedData);
+    }
+
     const reactTutorials = await searchVideos("react tutorial");
     const data = reactTutorials.videos.map((video) => ({
       id: video.id,
       title: video.title,
       url: `https://www.youtube.com/watch?v=${video.id}`,
     }));
+    
     if (!data || data.length === 0)
       return res.status(404).json({ message: "Videos not found" });
+
+    await redisClient.set("reactVideosCache", JSON.stringify(data));
+
     return res.status(200).json(data);
   } catch (err) {
-    return res
-      .status(500)
-      .json({
-        message: "Error when fetching React videos",
-        error: err.message,
-      });
+    return res.status(500).json({
+      message: "Error when fetching React videos",
+      error: err.message,
+    });
   }
 };
